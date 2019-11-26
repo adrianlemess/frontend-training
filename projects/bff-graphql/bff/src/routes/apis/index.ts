9bc23cf8c67075af77b25592c8abbf6c32b3e76b
Adrian Lemes <adrianlemess@gmail.com>
2019-11-26 13:09:16 -0300

    update bff package

--- projects/bff-graphql/bff/src/routes/apis/index.ts
+++ projects/bff-graphql/bff/src/routes/apis/index.ts
@@ -4,5 +4,6 @@ import userRouter from './users';
 const router = express.Router();
 
 router.use('/users-modified', userRouter);
+// create your parents route here
 
 export default router;
--- projects/bff-graphql/bff/src/routes/index.ts
+++ projects/bff-graphql/bff/src/routes/index.ts
@@ -1,17 +1,12 @@
 import { Request, Response } from 'express';
 import apis from './apis';
-import ProxyController from '../controllers/proxy';
 
 export class Routes {
-    private proxyController: ProxyController;
     constructor() {
-        this.proxyController = new ProxyController();
     }
 
     public routes(app): void {          
-        app.use('/custom', apis)
-        app.all('/graphql', this.proxyController.getProxyGraphQL)
-        app.all('/api/*', this.proxyController.getProxyAPI)
+        app.use('/api', apis)
 
         app.route('/health')
         .get((req: Request, res: Response) => {         
