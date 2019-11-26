b1797250abf0a186376a4a8b75b7172cd32310c2
Adrian Lemes <adrianlemess@gmail.com>
2019-11-26 16:56:09 -0300

    update route

--- projects/angular-ssr/src/app/app-routing.module.ts
+++ projects/angular-ssr/src/app/app-routing.module.ts
@@ -6,8 +6,7 @@ import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', component: DashboardComponent, pathMatch: 'full' },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent }
 ];
